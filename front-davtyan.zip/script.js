63536479578d768abef9de56d61a384b33105e34
Arsen-DavtyanGid <124453173+Arsen-DavtyanGid@users.noreply.github.com>
2023-02-05 21:26:30 +0400

    Update script.js

--- front-davtyan.zip/script.js
+++ front-davtyan.zip/script.js
@@ -1,14 +1,12 @@
 {
   const nav = document.querySelector("nav");
-  let lastScrollY = window.scrollY;
-  console.log(lastScrollY);
+  let lastScrollY = window.scrollY + 200;
+
   window.addEventListener("scroll", () => {
     if (lastScrollY < window.scrollY) {
       nav.classList.add("nav--hidden");
     } else {
       nav.classList.remove("nav--hidden");
     }
-
-    lastScrollY = window.scrollY;
   });
 }
